1b07b257b609cf3829d8e9b045bb0c353b037f7b
Agustin De Battista <71238269+agusdeba@users.noreply.github.com>
2023-09-28 14:37:30 -0300

    login inicial v2

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try{
+            $request->validate(
+                [
+                    'usuario' => 'required|string',
+                    'password' => 'required|string',
+                    'email' => 'required|email|unique:empleado,email',
+                    
+                ],
+                [
+                    'usuario.required' => 'El usuario no puede ser vacío',
+                    'usuario.string' => 'El usuario no tiene el formato adecuado.',
+    
+                    'password.required' => 'El password no puede ser vacío.',
+                    'password.string' => 'El password no tiene el formato adecuado.',
+
+                    'email.required' => 'El email no puede ser vacío.',
+                    'email.email' => 'El email no tiene el formato adecuado.',
+                    'email.unique' => 'Ya existe un empleado registrado con el email ingresado.',
+                ]
+            );
+
+            $admin = new User();
+
+            $admin -> usuario = $request -> get('usuario'); 
+            $admin -> password = $request -> get('password'); 
+            $admin -> email = $request -> get('email'); 
+
+            $admin->save();
+            $admin->setHidden(['created_at', 'updated_at']);
+            
+        }
+        catch(ValidationException $e) {
+            $errors = $e->validator->errors()->all();
+            
+            return redirect()->back()->withInput()->withErrors($errors);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
