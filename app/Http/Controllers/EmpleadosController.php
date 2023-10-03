2b0632c9e2bf8fb7c536aebf74291f2f1190e7e7
albanomazzino <albimazzino@gmail.com>
2023-10-03 16:49:59 -0300

    Fix -> Unique DNI per empleado (controller validation)

--- app/Http/Controllers/EmpleadosController.php
+++ app/Http/Controllers/EmpleadosController.php
@@ -41,7 +41,7 @@ class EmpleadosController extends Controller
                     'nombre' => 'required|string|max:50',
                     'apellido' => 'required|string|max:50',
                     'fecha_nacimiento' => 'required|date',
-                    'dni' => 'required|numeric|min:1|max:99999999',
+                    'dni' => 'required|numeric|min:1|max:99999999|unique:empleados,dni',
                     'email' => 'required|email|unique:empleados,email',
                     'direccion' => 'required|string|max:100',
                     'telefono' => 'required|numeric',
@@ -63,6 +63,7 @@ class EmpleadosController extends Controller
                     'dni.numeric' => 'El DNI no tiene el formato adecuado.',
                     'dni.min' => 'El DNI ingresado tiene que ser mayor que 0.',
                     'dni.max' => 'El DNI ingresado es más extenso de lo permitido (8 dígitos).',
+                    'dni.unique' => 'Ya existe un empleado registrado con el DNI ingresado.',
 
                     'email.required' => 'El email no puede ser vacío.',
                     'email.email' => 'El email no tiene el formato adecuado.',
