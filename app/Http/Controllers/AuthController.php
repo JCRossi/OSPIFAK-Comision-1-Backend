a3f24289b5f5555464b59b2ce396e7537e14d1d9
Agustin De Battista <71238269+agusdeba@users.noreply.github.com>
2023-10-08 19:40:34 -0300

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login($usuario, $password)
+    public function login(Request $request)
     {
+        $usuario = $request->input('usuario');
+    $password = $request->input('password');
+
         $cliente = DB::table('clientes')
             ->where('usuario', $usuario)
             ->where('password', $password)
