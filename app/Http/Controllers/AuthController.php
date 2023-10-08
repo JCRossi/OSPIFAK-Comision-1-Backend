653dd5a112eefdfa86273136c2c981b987eb2f97
Agustin De Battista <71238269+agusdeba@users.noreply.github.com>
2023-10-08 20:02:40 -0300

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,17 +10,16 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $usuario = $request->input('usuario');
-        $password = $request->input('password');
-
+       $password = $request->input('password');
+        
         $cliente = DB::table('clientes')
             ->where('usuario', $usuario)
-            ->where('password', $password)
             ->first();
 
         if ($cliente) {
             return response()->json(['message' => 'Autenticación exitosa']);
         } else {
-            return response()->json(['message' => 'Credenciales incorrectas'], 401);
+            return response()->json(['message' => 'Credenciales incorrectas '+$usuario+' '+$password], 401);
         }
     }
 }
