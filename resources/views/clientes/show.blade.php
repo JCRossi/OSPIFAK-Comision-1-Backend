6500b3036ae72155e1aa325d75557b4267c4f069
valenbarrera <89612680+valenbarrera@users.noreply.github.com>
2023-10-13 16:12:28 -0300

    Detalles

--- resources/views/clientes/show.blade.php
+++ resources/views/clientes/show.blade.php
@@ -7,6 +7,7 @@
             <div class="input-group mb-3 ">
                 <label class="form-label text" style="font-size: x-large; color: #78d278;">Titular</label>
             </div>
+
             <div class="row">
                 <div class="col-md-6">
                     <div class="input-group mb-3">
@@ -26,7 +27,7 @@
 
                     <div class="input-group mb-3">
                         <span class="input-group-text" style= "background-color: white;">Plan:</span>
-                        <span class="input-group-text" style= "font-size: x-large; background-color: white;">{{$cliente->plan_id}}</span>
+                        <span class="input-group-text" style= "font-size: x-large; background-color: white;">{{$cliente->plan_nombre}}</span>
                     </div>
                     
                 </div>
@@ -60,34 +61,39 @@
             </div>
 
             @php
-                function calcularEdad($fechaNacimiento) {
-                    $fechaActual = date('Y-m-d');
-                    $edad = date_diff(date_create($fechaNacimiento), date_create($fechaActual));
-                    return $edad->y;
-                }
+            function calcularEdad($fechaNacimiento) {
+                $fechaActual = date('Y-m-d');
+                $edad = date_diff(date_create($fechaNacimiento), date_create($fechaActual));
+                return $edad->y;
+            }
             @endphp
 
-            <table class="table table-striped mt-4">
-                <thead class="bg-primary text-white">
-                    <tr>
-                        <th scope="col">DNI</th>
-                        <th scope="col">Apellido y nombre</th>
-                        <th scope="col">Edad</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($menores as $menor)
-                    <tr>
-                        <td>{{ $menor->dni }}</td>
-                        <td>{{ $menor->apellido }} {{ $menor->nombre }}</td>
-                        <td>{{ calcularEdad($menor->fecha_nacimiento) }}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            @if(count($menores) > 0)
+                <table class="table table-striped mt-4">
+                    <thead class="bg-primary text-white">
+                        <tr>
+                            <th scope="col">DNI</th>
+                            <th scope="col">Apellido y nombre</th>
+                            <th scope="col">Edad</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($menores as $menor)
+                        <tr>
+                            <td>{{ $menor->dni }}</td>
+                            <td>{{ $menor->apellido }} {{ $menor->nombre }}</td>
+                            <td>{{ calcularEdad($menor->fecha_nacimiento) }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <p>No hay menores asociados a este cliente.</p>
+            @endif
             <div class="form-group">
-                <button type="submit" name="action" value="menor" class="btn btn-outline-success">Generar cupon de pago</button>
+                <button type="submit" name="action" value="menor" class="btn btn-outline-success">Generar cupón de pago</button>
             </div>
+
             <div class="form-group float-end">
                 <a href="/clientes" class="btn btn-outline-primary">Volver</a>
             </div>
