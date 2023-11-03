78707e67917288cd7190c14b0dce00fe0e10be6c
Juan Cruz Rossi <juanrossi1969@gmail.com>
2023-11-03 19:45:38 -0300

    Cambios API Cliente

--- app/Http/Controllers/ClientesControllerAPI.php
+++ app/Http/Controllers/ClientesControllerAPI.php
@@ -4,13 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Models\Cliente;
 use App\Models\Prestacion;
-use App\Models\Menor;
+use App\Models\Plan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use App\Models\Menor;
 use Carbon\Carbon;
+
 
 class ClientesControllerAPI extends Controller
 {
+     public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'usuario' => ['required','string'],
+            'password' => ['required','string']
+        ]);
+
+        if ($validator->fails()){
+            return response()->json(['errors' => $validator->errors()],401);
+        }
+
+       // if (!Auth::guard('clientes')->attempt($request->only(['usuario','password']))){
+         //   return response()->json(['error' => 'Invalid usuario or password'],401);
+       // }  
+
+        $cliente = Cliente::where('usuario', $request->usuario)->first();
+        $cliente->setHidden(['created_at', 'updated_at']);
+        $token = $cliente->createToken('myapptoken')->plainTextToken;
+
+        return response()->json([
+            'usuario' => $cliente,
+            'token' => $token
+        ],200);
+    }
+   
+
 
     public function registrar(Request $request)
     {
@@ -24,7 +55,7 @@ class ClientesControllerAPI extends Controller
                     'dni' => 'required|numeric|min:1|max:99999999|unique:clientes,dni',
                     'email' => 'required|email',
                     'direccion' => 'required|string|max:100',
-                    'telefono' => 'required|numeric',
+                    'telefono' => 'required|numeric|max:9999999999',
                 ],
                 [
                     'usuario.required' => 'El usuario no puede ser vacío',
@@ -59,6 +90,7 @@ class ClientesControllerAPI extends Controller
 
                     'telefono.required' => 'El telefono no puede ser vacío',
                     'telefono.numeric' => 'El telefono no tiene el formato adecuado.', 
+                    'telefono.max' => 'El telefono debe tener 10 digitos como maximo',
 
                  ]
             );
@@ -69,83 +101,220 @@ class ClientesControllerAPI extends Controller
             }
 
             $cliente = new Cliente();
-            $cliente -> usuario = $request->dni; 
-            $cliente -> password = $request->dni; 
-            $cliente -> nombre = $request->nombre; 
-            $cliente -> apellido = $request->apellido; 
-            $cliente -> fecha_nacimiento = $request->fecha_nacimiento; 
-            $cliente -> dni = $request->dni; 
-            $cliente -> email = $request->email; 
-            $cliente -> direccion = $request->direccion; 
-            $cliente -> telefono = $request -> telefono; 
-            $cliente -> plan_id = $request -> plan_id;
-            $cliente -> forma_pago = $request -> forma_pago;  
+            $cliente -> usuario = $request -> get('usuario'); 
+            $cliente -> password = $request -> get('password'); 
+            $cliente -> nombre = $request -> get('nombre'); 
+            $cliente -> apellido = $request -> get('apellido'); 
+            $cliente -> fecha_nacimiento = $request -> get('fecha_nacimiento'); 
+            $cliente -> dni = $request -> get('dni'); 
+            $cliente -> email = $request -> get('email'); 
+            $cliente -> direccion = $request -> get('direccion'); 
+            $cliente -> telefono = $request -> get('telefono'); 
+            $cliente -> plan_id = $request -> get('plan_id');
+            $cliente -> forma_pago = $request -> get('forma_pago'); 
             $cliente->save();
             $cliente->setHidden(['created_at', 'updated_at']);
+            
             return response()->json(['message' => 'El cliente fue creado con éxito'], 201);
     }
 
-    public function guardarPrestacion(Request $request){
-        $request -> validate(
+    public function datos(Request $request)
+    {
+        $usuario = $request->usuario;
+        // Buscar el cliente por usuario
+        $cliente = Cliente::where('usuario', $usuario)->first();
+
+        if (!$cliente) {
+            return response()->json(['error' => 'Cliente no encontrado'], 404);
+        }
+
+        // Obtener la información del cliente
+        $clienteInfo = [
+            'DNI' => $cliente->dni,
+            'nombre' => $cliente->nombre,
+            'apellido' => $cliente->apellido,
+            'fecha_nacimiento' => $cliente->fecha_nacimiento,
+            'email' => $cliente->email,
+            'direccion' => $cliente->direccion,
+            'telefono' => $cliente->telefono,
+        ];
+
+        // Verificar si existen clientes menores asociados
+        $clientesMenores = Menor::where('cliente_id', $cliente->id)->get();
+
+        if ($clientesMenores->count() > 0) {
+            // Agregar información de los clientes menores al resultado
+            $clienteInfo['clientes_menores'] = $clientesMenores;
+        }
+
+        return response()->json($clienteInfo);
+    }    
+
+
+    public function registrarMenor(Request $request)
+    {
+        $request->validate(
             [
-                'nombre_medico' => 'required|string|max:50',
-                'matricula_medico' => 'required|string|max:50',
-                'instituto' => 'required|string|max:50',
-                'fecha_turno' => 'required|date',
-                'comentario' => 'max:500'
+                'nombre' => 'required|string|max:50',
+                'apellido' => 'required|string|max:50',
+                'fecha_nacimiento' => 'required|date',
+                'dni' => 'required|numeric|min:1|max:99999999|unique:cliente_menor,dni',
             ],
             [
-                'nombre_medico.required' => 'El nombre del médico no puede ser vacío',
-                'nombre_medico.string' => 'El nombre del médico no tiene el formato adecuado.',
-                'nombre_medico.max' => 'El nombre del médico ingresado es más extenso de lo permitido (50 caracteres).',
+                'nombre.required' => 'El nombre no puede ser vacío',
+                'nombre.string' => 'El nombre no tiene el formato adecuado.',
+                'nombre.max' => 'El nombre ingresado es más extenso de lo permitido (50 caracteres).',
 
-                'matricula_medico.required' => 'La matrícula no puede ser vacía',
-                'matricula_medico.string' => 'La matrícula no tiene el formato adecuado.',
-                'matricula_medico.max' => 'La matrícula ingresada es más extensa de lo permitido (50 caracteres).',
+                'apellido.required' => 'El apellido no puede ser vacío',
+                'apellido.string' => 'El apellido no tiene el formato adecuado.',
+                'apellido.max' => 'El apellido ingresado es más extenso de lo permitido (50 caracteres).',
                 
-                'instituto.required' => 'El nombre del instituto no puede ser vacío',
-                'instituto.string' => 'El nombre del instituto no tiene el formato adecuado.',
-                'instituto.max' => 'El nombre del instituto ingresado es más extenso de lo permitido (50 caracteres).',
+                'fecha_nacimiento.required' => 'La fecha de nacimiento no puede ser vacía.',
+                'fecha_nacimiento.date' => 'La fecha de nacimiento no tiene el formato adecuado.',
 
-                'fecha_turno.required' => 'La fecha del turno no puede ser vacía.',
-                'fecha_turno.date' => 'La fecha del turno no tiene el formato adecuado.',
+                'dni.required' => 'El DNI no puede ser vacío.',
+                'dni.numeric' => 'El DNI no tiene el formato adecuado.',
+                'dni.min' => 'El DNI ingresado tiene que ser mayor que 0.',
+                'dni.max' => 'El DNI ingresado es más extenso de lo permitido (8 dígitos).',
+                'dni.unique' => 'Ya existe un menor registrado con el email ingresado.',
 
-                'comentario.max' => 'El comentario ingresado es más extenso de lo permitido (500 caracteres).',
-
-             ]
+            ]
         );
 
-        $prestacion = new Prestacion();
-        $prestacion->cliente_id = $request->user()->id;
-        
-        if($request->cliente_menor != null)
-            $prestacion->cliente_menor_id = $request->cliente_menor->id;
-        else
-            $prestacion->cliente_menor_id = -1;
-        
-        $prestacion->nombre_medico = $request->nombre_medico;
-        $prestacion->matricula_medico = $request->matricula_medico;
-        $prestacion->instituto = $request->instituto;
-        $prestacion->fecha_turno = $request->fecha_turno;
-        $prestacion->fecha_solicitud = Carbon::now();
-        $prestacion->estado = "Pendiente";
-        $prestacion->nombre_prestacion = $request->nombre_prestacion;
-        $prestacion->comentario = $request->comentario;
-        $prestacion->save();
+        $clienteId = Cliente::max('id');
 
-        return response()->json(['message' => 'La solicitud se realizó con éxito'], 201);
+        $menor = new Menor();
+
+        $menor->cliente_id = $clienteId;
+        $menor->nombre = $request -> get('nombre'); 
+        $menor->apellido = $request -> get('apellido'); 
+        $menor->fecha_nacimiento = $request -> get('fecha_nacimiento'); 
+        $menor->dni = $request -> get('dni');
+
+        $menor->save();
+        $menor->setHidden(['created_at', 'updated_at']);
+    
+        return response()->json(['message' => 'El menor fue creado con éxito'], 201);
     }
 
     public function recuperarPrestaciones(Request $request){
-        $clienteId = $request->user()->id;
-        $prestaciones = Prestacion::where('cliente_id', $clienteId)->get();
+        $data = $request->json()->all();
+        $dniCliente = $data['dni'];
+    
+        $cliente = Cliente::where('dni', $dniCliente)->first();
+    
+        if (!$cliente) {
+            return response()->json(['error' => 'Cliente no encontrado'], 404);
+        }
+    
+        $prestaciones = Prestacion::select('prestaciones.estado', 'prestaciones.fecha_solicitud', 
+        DB::raw('(SELECT dni FROM clientes WHERE prestaciones.cliente_id = clientes.id) as cliente_dni'),
+        DB::raw('(SELECT nombre FROM clientes WHERE prestaciones.cliente_id = clientes.id) as cliente_nombre'),
+        DB::raw('(SELECT apellido FROM clientes WHERE prestaciones.cliente_id = clientes.id) as cliente_apellido'),
+        DB::raw('(SELECT dni FROM clientes WHERE prestaciones.cliente_menor_id = clientes.id) as cliente_menor_dni'),
+        DB::raw('(SELECT nombre FROM clientes WHERE prestaciones.cliente_menor_id = clientes.id) as cliente_menor_nombre'),
+        DB::raw('(SELECT apellido FROM clientes WHERE prestaciones.cliente_menor_id = clientes.id) as cliente_menor_apellido'))
+        ->get();
+
+
+
+
+        /*
+        $items = Item::select('item.id', 'nombre', 'etiqueta', 'precio', 'item.activo', 'path_imagen')
+                    ->leftJoin('tipo', function ($join) {
+                        $join->on('item.tipo_id', '=', 'tipo.id');
+                    })
+                    ->where('item.activo', 1)
+                    ->where('tipo.activo', 1)
+                    ->orderBy('item.id')
+                    ->paginate(10);
+        
+        $prestaciones = Prestacion::where('cliente_id', $cliente->id)->get();*/
+
         return response()->json($prestaciones);
     }
-
+    
     public function recuperarMenores(Request $request){
-        $clienteId = $request->user()->id;
-        $menores = Menor::where('cliente_id', $clienteId)->get();
+        $data = $request->json()->all();
+        $dniCliente = $data['dni'];
+    
+        $cliente = Cliente::where('dni', $dniCliente)->first();
+    
+        if (!$cliente) {
+            return response()->json(['error' => 'Cliente no encontrado'], 404);
+        }
+    
+        $menores = Menor::where('cliente_id', $cliente->id)->get();
         return response()->json($menores);
+    }
+
+    public function recuperarPlan(Request $request){
+        $data = $request->json()->all();
+        $dniCliente = $data['dni'];
+    
+        $cliente = Cliente::where('dni', $dniCliente)->first();
+    
+        if (!$cliente) {
+            return response()->json(['error' => 'Cliente no encontrado'], 404);
+        }
+    
+        $plan = Plan::where('id', $cliente->id)->first();
+        return response()->json($plan);
+    }
+    
+    public function guardarPrestacion(Request $request)
+    {
+        // Validación de los datos del formulario
+        /*$request->validate([
+            'dni' => 'required',
+            'profesional' => 'required',
+            'matricula' => 'required',
+            'tipoPrestacion' => 'required',
+            'instituto' => 'required',
+            'fechaTurno' => 'required|date',
+            'comentarios' => 'nullable',
+        ]);*/
+    
+        // Buscar el cliente en función del DNI proporcionado
+        $cliente = Cliente::where('dni', $request->input('dni'))->first();
+    
+        if (!$cliente) {
+            return response()->json(['error' => 'Cliente no encontrado'], 404);
+        }
+    
+        // Obtener el ID del cliente y del menor (si se seleccionó uno)
+        $clienteId = $cliente->id;
+        $clienteMenorId = null;
+    
+        if ($request->input('selectedClienteDNI') !== $request->input('dni')) {
+            // Si el cliente seleccionado es distinto del cliente principal, se usa el DNI del cliente seleccionado como cliente_menor_id
+            $clienteMenor = Menor::where('dni', $request->input('selectedClienteDNI'))->first();
+    
+            if ($clienteMenor) {
+                $clienteMenorId = $clienteMenor->id;
+            }
+        }
+        $fechaSolicitud = Carbon::now();
+        $estado = 'Pendiente';
+        // Crear una nueva instancia de Prestacion
+        $prestacion = new Prestacion();
+        $prestacion->cliente_id = $clienteId;
+        $prestacion->cliente_menor_id = $clienteMenorId;
+        $prestacion->nombre_medico = $request->input('profesional');
+        $prestacion->matricula_medico = $request->input('matricula');
+        $prestacion->instituto = $request->input('instituto');
+        $prestacion->fecha_turno = $request->input('fechaTurno');
+        $prestacion->estado = $estado; // Estado inicial
+        $prestacion->nombre_prestacion = $request->input('tipoPrestacion');
+        $prestacion->comentario = $request->input('comentarios');
+        $prestacion->fecha_solicitud = $fechaSolicitud;
+    
+        // Guardar la prestación en la base de datos
+        $prestacion->save();
+    
+        // Puedes realizar acciones adicionales después de guardar la prestación
+    
+        return response()->json(['message' => 'Prestación guardada con éxito'], 201);
     }
 
      /**
