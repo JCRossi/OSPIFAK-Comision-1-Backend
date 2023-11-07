31ab06763c3d8b998270413a457bf04283d8e2a8
albanomazzino <albimazzino@gmail.com>
2023-11-07 17:18:55 -0300

    Fix datos API (ClientesControllerAPI)

--- app/Http/Controllers/ClientesControllerAPI.php
+++ app/Http/Controllers/ClientesControllerAPI.php
@@ -170,7 +170,7 @@ class ClientesControllerAPI extends Controller
         }
 
         return response()->json($clienteInfo);
-    }    
+    }  
 
 /*
     public function registrarMenor(Request $request)
@@ -436,39 +436,6 @@ class ClientesControllerAPI extends Controller
         return response()->json(['message' => 'Baja solicitada con éxito'], 200);
     }
 
-*/
-/*
-    public function datos(Request $request)
-    {
-        $usuario = $request->usuario;
-        // Buscar el cliente por usuario
-        $cliente = Cliente::where('usuario', $usuario)->first();
-
-        if (!$cliente) {
-            return response()->json(['error' => 'Cliente no encontrado'], 404);
-        }
-
-        // Obtener la información del cliente
-        $clienteInfo = [
-            'DNI' => $cliente->dni,
-            'nombre' => $cliente->nombre,
-            'apellido' => $cliente->apellido,
-            'fecha_nacimiento' => $cliente->fecha_nacimiento,
-            'email' => $cliente->email,
-            'direccion' => $cliente->direccion,
-            'telefono' => $cliente->telefono,
-        ];
-
-        // Verificar si existen clientes menores asociados
-        $clientesMenores = Menor::where('cliente_id', $cliente->id)->get();
-
-        if ($clientesMenores->count() > 0) {
-            // Agregar información de los clientes menores al resultado
-            $clienteInfo['clientes_menores'] = $clientesMenores;
-        }
-
-        return response()->json($clienteInfo);
-    }    
 */
 
     public function registrarMenor(Request $request)
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientesController;
 use App\Http\Controllers\SolicitudesBajaControllerAPI;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,7 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::post('/login', [ClientesControllerAPI::class, 'login']);
-Route::get('/datos', [AuthController::class, 'datos']);
+Route::get('/datos', [ClientesControllerAPI::class, 'datos']);
 Route::post('/registrar', [ClientesControllerAPI::class, 'registrar']);
 //Route::get('/planes', [PlanesControllerAPI::class, 'index']);
 
